2dc87c5604498a187a54bba840f1c272aef1ea97
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-13 11:04:09 -0500

    first commit

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here
+ARTICLE_STATUS = (
+    ("draft","draft"),
+    ("inprogress", "inprogress"),
+    ("published", "oublished"),
+)
+
+class UserProfile(AbstractUser):
+    pass
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default="")
+    word_count = models.IntegerField()
+    x_post = models.TextField(blank=True, default="")
+    status = models.CharField(
+        max_length=20,
+        choices=ARTICLE_STATUS,
+        default="draft"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
