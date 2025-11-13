a82274cacac2b548e21a80a1e8cecdd1b33c6846
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-13 11:23:08 -0500

    Chore: add templating

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from app.models import Article
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    articles = Article.objects.all()
+    return render(request, "app/home.html", {"articles": articles})
+
 
 
