32f33ef4834666b32f332002949540814e183542
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-13 21:54:59 -0500

    Chore: added other endpoints in CBV's

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from app.views import home, ArticleCreateView
+from app.views import ArticleCreateView, ArticleUpdateView, ArticleListView, ArticleDeleteView
 
 urlpatterns = [
-    path("",home, name="home"),
-    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
+    path("",ArticleListView.as_view(), name="home"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("<int:pk>/update", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete", ArticleDeleteView.as_view(), name="delete_article"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 #from app.forms import CreateArticleForm
 from app.models import Article
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 # Create your views here.
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "app/home.html", {"articles": articles})
+# def home(request):
+#     articles = Article.objects.all()
+#     return render(request, "app/home.html", {"articles": articles})
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
 
 class ArticleCreateView(CreateView):
     template_name = "app/article_create.html"
@@ -17,9 +21,20 @@ class ArticleCreateView(CreateView):
     success_url = reverse_lazy("home")
 
 
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "x_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
 
 
 
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
 
 # Function base view  
 # def create_article(request):
