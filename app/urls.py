af13d0a3b30d903819e04067cb3d7272f71e9e18
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-13 19:51:30 -0500

    Chore: changed from function based views to class  based

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from app.views import home
+from app.views import home, ArticleCreateView
 
 urlpatterns = [
-    path("",home),
+    path("",home, name="home"),
+    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+#from app.forms import CreateArticleForm
 from app.models import Article
+from django.views.generic import CreateView
 
 # Create your views here.
 def home(request):
     articles = Article.objects.all()
     return render(request, "app/home.html", {"articles": articles})
 
+class ArticleCreateView(CreateView):
+    template_name = "app/article_create.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "x_post"]
+    success_url = reverse_lazy("home")
 
 
+
+
+
+
+# Function base view  
+# def create_article(request):
+#     if request.method == "POST":
+#         form = CreateArticleForm(request.POST)
+#         if form.is_valid():
+#             form_data = form.cleaned_data
+#             new_article = Article(
+#                 title = form_data["title"],
+#                 status = form_data["status"],
+#                 content = form_data["content"],
+#                 word_count = form_data["word_count"],
+#                 x_post = form_data["x_post"],
+#             )
+#             new_article.save()
+
+#             return redirect("home")
+#     else: 
+#         form = CreateArticleForm()
+#     return render(request, "app/article_create.html", {"form": form})
