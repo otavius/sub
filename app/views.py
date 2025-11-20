627700f0f91ee4e4690324f2e001db4b094fd0fd
Otavius Whitehead <developer.otavius@gmail.com>
2025-11-20 10:12:08 -0500

    chore: set up django-allauth

--- app/views.py
+++ app/views.py
@@ -1,20 +1,25 @@
+#from typing import Any
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 #from app.forms import CreateArticleForm
 from app.models import Article
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 # def home(request):
 #     articles = Article.objects.all()
 #     return render(request, "app/home.html", {"articles": articles})
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
 
-class ArticleCreateView(CreateView):
+    def get_queryset(self):
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title", "status", "content", "x_post"]
@@ -26,20 +31,45 @@ class ArticleCreateView(CreateView):
     
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     template_name = "app/article_update.html"
     model = Article
     fields = ["title", "status", "content", "x_post"]
     success_url = reverse_lazy("home")
     context_object_name = "article"
 
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "app/article_delete.html"
     model = Article
     success_url = reverse_lazy("home")
     context_object_name = "article"
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Function base view  
 # def create_article(request):
